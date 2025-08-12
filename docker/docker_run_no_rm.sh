140e420a0c9f88bf1a91b981b4d77c8f171cd36c
marco_dataiku <marcochang1028@gmail.com>
2025-08-12 15:22:43 +0800

    改port與移除行尾問題

--- docker/docker_run_no_rm.sh
+++ docker/docker_run_no_rm.sh
@@ -2,6 +2,6 @@ docker run --gpus all -d --name qwen2.5-omni-7b \
     -v /etc/localtime:/etc/localtime:ro \
     -v /etc/timezone:/etc/timezone:ro \
     -v ~/.cache/huggingface:/root/.cache/huggingface \
-    -p 5003:5000 \
+    -p 5005:5000 \
     --network llm_network \
     qwen2.5-omni-7b
